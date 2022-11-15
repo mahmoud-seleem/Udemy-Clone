33afb5ef68141926716ae4f318ddd129f50eef88
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2022-11-15 19:26:04 +0200

    fix: fixed the view of the login fragment in the sign Activity

--- app/src/main/java/com/example/platform/MainActivity.java
+++ app/src/main/java/com/example/platform/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.platform;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+//        SharedPreferences preferences = getSharedPreferences("sharedPreferencesFile", MODE_PRIVATE);
+//        boolean isTheFirstAppRun = preferences.getBoolean("FirstAppRun", true);
+//        if (isTheFirstAppRun) {
+//            SharedPreferences.Editor preferencesEditor = (SharedPreferences.Editor) getSharedPreferences("sharedPreferencesFile", MODE_PRIVATE);
+//            preferencesEditor.putBoolean("FirstAppRun", false).commit(); //commit()
+//
+//            launchSignActivity();
+//            finish(); //finish first main activity //main -> sign -> main =>>> main(finish) -> sign -> main
+//        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public void launchSignActivity() {
+        Intent switchToSignActivity = new Intent(MainActivity.this, SignActivity.class);
+        startActivity(switchToSignActivity);
     }
 }
